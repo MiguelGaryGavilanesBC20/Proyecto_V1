f0614f9e3c050f99da477087f9a215d54f223289
MiguelGaryGavilanesBC20 <proyectoguipoo2024@gmail.com>
2024-12-20 23:40:54 -0500

    Proyecto de POO1

---
+++ src/Formulario2.java
@@ -0,0 +1,112 @@
+package Menu_P;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.*;
+
+public class Formulario2 extends JFrame implements ActionListener {
+
+    private JButton boton1, boton2;
+    private JLabel label1, label2, label3;
+    private JTextField texto1, texto2, texto3;
+    private JTable tabla;
+    private DefaultTableModel modeloTabla;
+    private JScrollPane scrollPane;
+
+    public Formulario2() {
+        setUndecorated(true);//eliminar botones de minimizar,maximizar,salir
+		setLayout(null);
+
+        
+        label1 = new JLabel("Nombre");
+        label1.setBounds(50, 20, 50, 20);
+        add(label1);
+
+        
+        texto1 = new JTextField();
+        texto1.setBounds(110, 20, 110, 20);
+        add(texto1);
+
+        
+        label2 = new JLabel("Apellido");
+        label2.setBounds(50, 50, 50, 20);
+        add(label2);
+
+        
+        texto2 = new JTextField();
+        texto2.setBounds(110, 50, 110, 20);
+        add(texto2);
+
+		
+		label3 = new JLabel("Equipo");
+        label3.setBounds(50, 80, 50, 20);
+        add(label3);
+
+        
+        texto3 = new JTextField();
+        texto3.setBounds(110, 80, 110, 20);
+        add(texto3);
+
+        
+        boton1 = new JButton("Guardar");
+        boton1.setBounds(300, 20, 100, 30);
+        add(boton1);
+        boton1.addActionListener(this);
+
+        
+        boton2 = new JButton("Salir");
+        boton2.setBounds(300, 60, 100, 30);
+        add(boton2);
+        boton2.addActionListener(this);
+
+        
+        modeloTabla = new DefaultTableModel();
+        modeloTabla.addColumn("Nombre");
+        modeloTabla.addColumn("Apellido");
+		modeloTabla.addColumn("Equipo");
+
+        
+        tabla = new JTable(modeloTabla);
+        scrollPane = new JScrollPane(tabla);
+        scrollPane.setBounds(50, 120, 400, 240);
+        add(scrollPane);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == boton1) { //guardar
+            
+            String nombre = texto1.getText();
+            String apellido = texto2.getText();
+			String equipo = texto3.getText();
+
+            
+            if (nombre.isEmpty() || apellido.isEmpty() || equipo.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Por favor, complete todos los campos.", "Error al agregar los datos", JOptionPane.ERROR_MESSAGE);
+            } else {
+                
+                modeloTabla.addRow(new Object[]{nombre, apellido, equipo});
+
+                //limpiar campos de texto
+                texto1.setText("");
+                texto2.setText("");
+				texto3.setText("");
+            }
+        }
+
+        if (e.getSource() == boton2) {//regresar
+            dispose();
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            Formulario2 formulario1 = new Formulario2();
+            formulario1.setBounds(0, 0, 500, 400);
+            formulario1.setVisible(true);
+            formulario1.setResizable(false);
+            formulario1.setLocationRelativeTo(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/MenuFrame.java
@@ -0,0 +1,138 @@
+package Menu_P;
+
+//import java.awt.Container;
+import java.awt.event.*;
+
+import javax.swing.*;
+//import javax.swing.JMenu;
+//import javax.swing.JMenuBar;
+//import javax.swing.JMenuItem;
+//import javax.swing.JPanel;
+
+//import javax.swing.JFrame;
+
+
+class MenuFrame extends JFrame implements ActionListener{
+    private JMenuItem info_p,participantes;
+    private JMenuBar mibarra;
+    private JMenuItem crear,salir,guardar,guardarcomo;
+    private JMenu acerca;
+    private JMenu archivo;
+    private JMenu editar;
+    private JMenu logo;
+
+    public MenuFrame() {
+        setLayout(null);
+        setBounds(0, 0, 550, 400);
+
+// MenuLamina milamina = new MenuLamina();
+
+// add(milamina);
+        mibarra = new JMenuBar();
+
+        archivo = new JMenu("Archivo");
+        editar  = new JMenu("Editar");
+        acerca  = new JMenu("Acerca de...");
+        logo    = new JMenu(" ");
+
+        crear = new JMenuItem("Nuevo");
+        crear.addActionListener(this);
+
+        salir = new JMenuItem("Salir");
+        salir.addActionListener(this);
+
+        guardar = new JMenuItem("Guardar");
+        guardar.addActionListener(this);
+
+        guardarcomo = new JMenuItem("Guardar como");
+        guardarcomo.addActionListener(this);
+
+        info_p = new JMenuItem("Informacion del Programa");
+        info_p.addActionListener(this);
+
+        participantes = new JMenuItem("Autores");
+        participantes.addActionListener(this);
+
+        JMenu consulta = new JMenu("Consulta");
+        JMenuItem consult_op1 = new JMenuItem("Jugadores");
+        JMenuItem consult_op2 = new JMenuItem("Equipos");
+        JMenuItem consult_op3 = new JMenuItem("Arbitros");
+        JMenuItem consult_op4 = new JMenuItem("Dirigentes");
+
+        JMenuItem actualizar = new JMenuItem("Actualizar");
+        JMenuItem borrar = new JMenuItem("Borrar");
+
+
+        mibarra.add(logo);
+        mibarra.add(archivo);
+        mibarra.add(editar);
+        mibarra.add(acerca);
+        
+        
+
+        archivo.add(crear);
+        archivo.add(salir);
+        archivo.add(guardar);
+        archivo.add(guardarcomo);
+        
+
+        editar.add(consulta);
+        consulta.add(consult_op1);
+        consulta.add(consult_op2);
+        consulta.add(consult_op3);
+        consulta.add(consult_op4);
+
+        editar.add(actualizar);
+        editar.add(borrar);
+        
+
+        acerca.add(info_p);
+        acerca.add(participantes);
+        
+
+
+
+//mibarra.add(opciones );
+
+        add(mibarra);
+
+//Indicamos que es el menu por defecto
+        setJMenuBar(mibarra);
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+// Container C=this.getContentPane();
+        
+        if(e.getSource()==crear){
+            Formulario2 ventanacrear = new Formulario2();
+            ventanacrear.setBounds(0,0,500,400);
+            ventanacrear.setVisible(true);
+            ventanacrear.setResizable(false);
+            ventanacrear.setLocationRelativeTo(null);
+
+        }
+        if(e.getSource()==salir){
+            System.exit(0);
+        }
+
+        if (e.getSource() == info_p) {
+            // Mostrar un mensaje en pantalla
+            JOptionPane.showMessageDialog(this, 
+                " Programa de campeonato creado por el Grupo # \n Materia: Programacion Orientada a Objetos.",
+                " Materia impartida por el Ing Crespo Mendoza Roberto",
+                JOptionPane.INFORMATION_MESSAGE);
+        }
+        if (e.getSource() == participantes) {
+            // Mostrar un mensaje en pantalla
+            JOptionPane.showMessageDialog(this, 
+                " Jose Agapito Hernandez Vega \n Miguel Eduardo Loyola Mora \n Gary Johao Zuñiga Saltos \n Cristhian Alejandro Gavilanes Sanchez \n Christian Alexander Bone Arias",
+                " Autores",
+                JOptionPane.INFORMATION_MESSAGE);
+        }
+
+
+    }
+
+
+}
